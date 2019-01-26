fb37073cda66cde07a80d1c6c390673e6bdbbf55
Naman Bhargava <namanb.edu@gmail.com>
2019-01-26 14:40:15 -0500

    moved company task theme and integrated it with company profile

--- client/src/containers/CompanyProfile.js
+++ client/src/containers/CompanyProfile.js
@@ -1,10 +1,115 @@
 import React, { Component } from 'react';
 
+// Make sure to only pull data from the database that matches this company
+const data = [
+    {
+        Company: "YeEt DoRiTe",
+        Title:"yeet the yote",
+        Wage: 123,
+        Description: "This is an amazing description to describe the description of the task that the company wishes to be done completely, well and better than ever before",
+        Location: "",
+        Category: "Website Design",
+        Date: "1/11/19"
+    },
+    {
+        Company: "YeEt DoRiTe",
+        Title:"yeet the yote",
+        Wage: 123,
+        Description: "This is an amazing description to describe the description of the task that the company wishes to be done completely, well and better than ever before",
+        Location: "",
+        Category: "Website Design",
+        Date: "1/11/19"
+    },
+    {
+        Company: "YeEt DoRiTe",
+        Title:"yeet the yote",
+        Wage: 123,
+        Description: "This is an amazing description to describe the description of the task that the company wishes to be done completely, well and better than ever before",
+        Location: "",
+        Category: "Website Design",
+        Date: "1/11/19"
+    }
+]
+
 class CompanyProfile extends Component {
   render() {
     return (
         <div className="CompanyProfile">
             CompanyProfile
+            <div className="ChallengeList pattern-bg">
+                <div className="ui grid container page-height">
+                    <div className="four wide teal column">
+                        <h4 className="ui header">Company Profile Navbar</h4>
+                    </div>
+                    <div className="twelve wide white column scroll-list">
+                        <h2 className="ui header">Current Tasks</h2>
+                        <div className="ui divider"></div>
+                            <div className="ui three stackable cards centered">
+                            {data.map((d) => (
+                                <div className="card">
+                                    <div className="content">
+                                        <div className="header">{d.Title}</div>
+                                        <div className="meta">
+                                            <span className="price left floated">${d.Wage}</span>
+                                            <span className="stay right floated">Posted On: {d.Date} </span>
+                                        </div>
+                                        <div className="description ui">
+                                        <span >{d.Description}</span>
+                                        </div>
+                                    </div>
+                                    <div className="extra content">
+                                            <div className = "ui two buttons">
+                                                <div class="ui attached button">
+                                                    <i class="newspaper outline icon"></i>
+                                                    View Task
+                                                </div>
+                                                <div class="ui attached button">
+                                                    <i class="edit icon"></i>
+                                                    Edit Task
+                                                </div>
+                                            </div>
+                                        </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+                </div>
+
+
+            <div className="ui centered container">
+                <div className="ui placeholder segment">
+                    <div className="ui two column stackable center aligned grid">
+                        <div className="ui vertical divider">Or</div>
+                        <div className="middle aligned row">
+                        <div className="column">
+                            <div className="ui icon header">
+                            <i className="search icon"></i>
+                            Find Students
+                            </div>
+                            <div className="field">
+                            <div className="ui search">
+                                <div className="ui icon input">
+                                <input className="prompt" type="text" placeholder="Search names, skills, ..."></input>
+                                <i className="search icon"></i>
+                                </div>
+                                <div className="results"></div>
+                            </div>
+                            </div>
+                        </div>
+                        <div className="column">
+                            <div className="ui icon header">
+                            <i className="file outline icon"></i>
+                            Add New Task
+                            </div>
+                            <div className="ui primary button">
+                            Create
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
   }
