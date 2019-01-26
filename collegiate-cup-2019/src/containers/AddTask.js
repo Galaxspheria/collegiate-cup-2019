41e5c0df7c500f70749e35c2926380a8782b97a9
Naman Bhargava <namanb.edu@gmail.com>
2019-01-26 09:56:03 -0500

    Implemented the add task content

--- collegiate-cup-2019/src/containers/AddTask.js
+++ collegiate-cup-2019/src/containers/AddTask.js
@@ -1,10 +1,46 @@
 import React, { Component } from 'react';
+import { database } from 'firebase';
+
+const categories = [
+    "Website Design", 
+    "Android App Development", 
+    "IOS App development", 
+    "Data Processing"
+]
 
 class AddTask extends Component {
   render() {
     return (
         <div className="AddTask">
-          Add Task
+            <div className="ui center container">
+                <div className="ui form">
+                    <div className="two fields">
+                        <div className="field">
+                            <label>Task Title</label>
+                            <input type="text" placeholder="Task Title"></input>
+                        </div>
+                        <div className="ui form">
+                            <div class="field">
+                                <label>Category</label>
+                                <select class="ui search dropdown">
+                                    <option value="">Select Category</option>
+                                    {categories.map((category)=> (
+                                        <option value="AF">{category}</option>
+                                    ))}
+                                </select>
+                            </div>
+                            </div>
+                    </div>
+                    <div className="field">
+                        <label>Quick Description</label>
+                        <textarea rows="2"></textarea>
+                    </div>
+                    <div className="field">
+                        <label>Skills</label>
+                        <textarea rows="2" placeholder="Separate skills with commas"></textarea>
+                    </div>
+                </div>
+            </div>
         </div>
     );
   }
