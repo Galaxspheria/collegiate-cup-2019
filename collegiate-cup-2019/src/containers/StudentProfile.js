d4cda701f87f02a3406c2584aa274899343c0f33
Lucas Kiefer <keekskiefer@gmail.com>
2019-01-26 02:21:24 -0500

    Student Profile update

--- collegiate-cup-2019/src/containers/StudentProfile.js
+++ collegiate-cup-2019/src/containers/StudentProfile.js
@@ -4,7 +4,25 @@ class StudentProfile extends Component {
   render() {
     return (
         <div className="StudentProfile">
-            StudentProfile
+          <div class="ui equal width centered grid">
+              <div className="column">
+                <img src="/images/image.png" height="300px" width="300px"></img>
+              </div>
+              <div class="twelve wide column">
+                <div class="ui segment">
+                Lucas Kiefer
+                <br></br>
+                High School: Fariview HS
+                <br></br>
+                Age: 17
+                <br></br>
+                Tier: 5
+                <br></br>
+                Speciality: Not Web dev lol
+                </div>
+              </div>
+          </div>
+          
         </div>
     );
   }
