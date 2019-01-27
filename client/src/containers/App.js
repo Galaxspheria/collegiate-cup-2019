1709f0011ccec663a74596db0f121c823886bdf3
Hunter Hancock <hunterghancock@gmail.com>
2019-01-26 22:39:43 -0500

    Added basic user state tracking through Context

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -17,45 +17,63 @@ import Login from './Login';
 import WriteReport from './WriteReport';
 import TaskProfile from './TaskProfile';
 
+import {UserContext} from './UserContext';
+
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      userID: 'Sa871ME9#2peR91C6X',
+      updateUserID: (id) => this.updateUserID(id)
+    }
+  }
+
+  updateUserID(id) {
+    this.setState({userID: id})
+  }
+
+  static contextType = UserContext;
+
   render() {
     return (
       <div className="App">
-        <Router>
-          <div>
-            <nav class="ui secondary pointing menu teal inverted" style={{position: "relative", zIndex: 10}}>
-              <NavLink className="item" activeClassName="item active" to="/" exact>Home</NavLink>
-              <NavLink className="item" activeClassName="item active" to="/challenges/" exact> Challenges</NavLink>
-              <NavLink className="item" activeClassName="item active" to="/dashboard/student/">Student Dashbo</NavLink>
-              <NavLink className="item" activeClassName="item active" to="/profile/organization/" exact>Org list</NavLink>
-              <NavLink className="item" activeClassName="item active" to="/dashboard/organization/">Organization Dashbo</NavLink>
-              <NavLink className="item" activeClassName="item active" to="/profile/student/" >Student List</NavLink>
-              <NavLink className="item" activeClassName="item active" to="/login/">Login</NavLink>
-              <NavLink className="item" activeClassName="item active" to="/WriteReport/">WriteReport</NavLink>
+        <UserContext.Provider value={this.state}>
+          <Router>
+            <div>
+              <nav class="ui secondary pointing menu teal inverted" style={{position: "relative", zIndex: 10}}>
+                <NavLink className="item" activeClassName="item active" to="/" exact>Home</NavLink>
+                <NavLink className="item" activeClassName="item active" to="/challenges/" exact> Challenges</NavLink>
+                <NavLink className="item" activeClassName="item active" to="/dashboard/student/">Student Dashbo</NavLink>
+                <NavLink className="item" activeClassName="item active" to="/profile/organization/" exact>Org list</NavLink>
+                <NavLink className="item" activeClassName="item active" to="/dashboard/organization/">Organization Dashbo</NavLink>
+                <NavLink className="item" activeClassName="item active" to="/profile/student/" >Student List</NavLink>
+                <NavLink className="item" activeClassName="item active" to="/login/">Login</NavLink>
+                <NavLink className="item" activeClassName="item active" to="/WriteReport/">WriteReport</NavLink>
 
-              <div className="right menu">
-                <a className="ui item">
-                  Logout
-                </a>
-              </div>
-            </nav>
+                <div className="right menu">
+                  <a className="ui item">
+                    Logout
+                  </a>
+                </div>
+              </nav>
 
-            <Route path="/" exact component={Home} />
-            <Route path="/home" component={Home} />
-            <Route path="/challenges/" exact component={ChallengeList} />
-            <Route path="/challenges/:id" component={Quiz} />
-            <Route path="/profile/student/" exact component={StudentList} />
-            <Route path="/profile/student/:id" component={StudentProfile} />
-            <Route path="/dashboard/student/" component={StudentTasks} />
-            <Route path="/profile/organization/" exact component={CompanyList} />
-            <Route path="/profile/organization/:id" component={OrganizationProfile} />
-            <Route path="/dashboard/organization/" component={OrganizationDashboard}/>
-            <Route path="/login/" component={Login} />
-            <Route path="/WriteReport/" component={WriteReport} />
-            <Route path="/profile/task/:id" component={TaskProfile} />
+              <Route path="/" exact component={Home} />
+              <Route path="/home" component={Home} />
+              <Route path="/challenges/" exact component={ChallengeList} />
+              <Route path="/challenges/:id" component={Quiz} />
+              <Route path="/profile/student/" exact component={StudentList} />
+              <Route path="/profile/student/:id" component={StudentProfile} />
+              <Route path="/dashboard/student/" component={StudentTasks} />
+              <Route path="/profile/organization/" exact component={CompanyList} />
+              <Route path="/profile/organization/:id" component={OrganizationProfile} />
+              <Route path="/dashboard/organization/" component={OrganizationDashboard}/>
+              <Route path="/login/" component={Login} />
+              <Route path="/WriteReport/" component={WriteReport} />
+              <Route path="/profile/task/:id" component={TaskProfile} />
 
-          </div>
-        </Router>
+            </div>
+          </Router>
+        </UserContext.Provider>
       </div>
     );
   }
--- client/src/containers/Login.js
+++ client/src/containers/Login.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import BGParticles from '../components/BGParticles';
 
+import {UserContext} from './UserContext';
+
 class Login extends Component {
     constructor(props) {
         super(props)
@@ -10,14 +12,18 @@ class Login extends Component {
         }
     }
 
+    static contextType = UserContext; // allows you to use this.context.userID in the render loop
+
     login() {
         const that = this;
         this.setState({loading: true})
         setTimeout(() => {
             const pw = this.refs.password.value.slice(0,1)[0];
             if (pw === "s" || pw === "S") {
+                that.context.updateUserID("Sa871ME9#2peR91C6X")
                 that.props.history.push("/dashboard/student/")
             } else if (pw === "c" || pw === "C") {
+                that.context.updateUserID("Ca871ME9#2peR91C6X")
                 that.props.history.push("/dashboard/organization/")
             } else {
                 that.setState({error: "Your username or password is incorrect.", loading: false})
@@ -26,50 +32,50 @@ class Login extends Component {
     }
 
     render() {
-    return (
-        <div className="Login">
-            <div className="ui middle aligned center aligned grid page-height container particle-window">
-                <div className="column centered">
-                    <div className="width-cap">
-                        {this.state.error?
-                            <div className="ui error message">
-                            <div className="header">
-                                Uh oh!
+        return (
+            <div className="Login">
+                <div className="ui middle aligned center aligned grid page-height container particle-window">
+                    <div className="column centered">
+                        <div className="width-cap">
+                            {this.state.error?
+                                <div className="ui error message">
+                                <div className="header">
+                                    Uh oh!
+                                </div>
+                                <p>{this.state.error}</p>
+                                </div>
+                            :null}
+                            <h2 className="ui teal image header">
+                            <div className="content">
+                                Sign In
                             </div>
-                            <p>{this.state.error}</p>
+                            </h2>
+                            <form className="ui large form">
+                            <div className="ui teal segment">
+                                <div className="field">
+                                <div className="ui left icon input">
+                                    <i className="user icon"></i>
+                                    <input type="text" name="email" placeholder="E-mail address"></input>
+                                </div>
+                                </div>
+                                <div className="field">
+                                <div className="ui left icon input">
+                                    <i className="lock icon"></i>
+                                    <input type="password" ref="password" name="password" placeholder="Password"></input>
+                                </div>
+                                </div>
+                                <div onClick={() => this.login()} className={this.state.loading? "ui fluid large teal submit button loading" : "ui fluid large teal submit button"}>Login</div>
                             </div>
-                        :null}
-                        <h2 className="ui teal image header">
-                        <div className="content">
-                            Sign In
-                        </div>
-                        </h2>
-                        <form className="ui large form">
-                        <div className="ui teal segment">
-                            <div className="field">
-                            <div className="ui left icon input">
-                                <i className="user icon"></i>
-                                <input type="text" name="email" placeholder="E-mail address"></input>
+                            </form>
+                            <div className="ui message">
+                            New student?&nbsp;&nbsp;<a href="#">Enter signup code!</a>
                             </div>
-                            </div>
-                            <div className="field">
-                            <div className="ui left icon input">
-                                <i className="lock icon"></i>
-                                <input type="password" ref="password" name="password" placeholder="Password"></input>
-                            </div>
-                            </div>
-                            <div onClick={() => this.login()} className={this.state.loading? "ui fluid large teal submit button loading" : "ui fluid large teal submit button"}>Login</div>
-                        </div>
-                        </form>
-                        <div className="ui message">
-                        New student?&nbsp;&nbsp;<a href="#">Enter signup code!</a>
                         </div>
                     </div>
                 </div>
+                <BGParticles/>
             </div>
-            <BGParticles/>
-        </div>
-    );
+        );
     }
 }
 
